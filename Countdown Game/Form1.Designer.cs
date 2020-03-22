44bd07e1825d9a275780f214a872d1c35136f883
OEM <OEM@DESKTOP-TSLDGJ4>
2020-03-22 20:59:39 +1300

    initial commit of full repository

---
+++ Countdown Game/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Countdown_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConsonant = new System.Windows.Forms.Button();
+            this.btnVowel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAnagram = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbTotalScore = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblRound = new System.Windows.Forms.Label();
+            this.txbRound = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbCurrentScore = new System.Windows.Forms.TextBox();
+            this.txbResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnFindResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConsonant
+            // 
+            this.btnConsonant.Location = new System.Drawing.Point(8, 96);
+            this.btnConsonant.Name = "btnConsonant";
+            this.btnConsonant.Size = new System.Drawing.Size(111, 23);
+            this.btnConsonant.TabIndex = 0;
+            this.btnConsonant.Text = "Consonant";
+            this.btnConsonant.UseVisualStyleBackColor = true;
+            this.btnConsonant.Click += new System.EventHandler(this.btnConsonant_Click);
+            // 
+            // btnVowel
+            // 
+            this.btnVowel.Location = new System.Drawing.Point(133, 96);
+            this.btnVowel.Name = "btnVowel";
+            this.btnVowel.Size = new System.Drawing.Size(75, 23);
+            this.btnVowel.TabIndex = 1;
+            this.btnVowel.Text = "Vowel";
+            this.btnVowel.UseVisualStyleBackColor = true;
+            this.btnVowel.Click += new System.EventHandler(this.btnVowel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "What is this word?";
+            // 
+            // txtAnagram
+            // 
+            this.txtAnagram.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnagram.Location = new System.Drawing.Point(8, 64);
+            this.txtAnagram.Name = "txtAnagram";
+            this.txtAnagram.ReadOnly = true;
+            this.txtAnagram.Size = new System.Drawing.Size(112, 26);
+            this.txtAnagram.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Total Score";
+            // 
+            // txbTotalScore
+            // 
+            this.txbTotalScore.Location = new System.Drawing.Point(78, 12);
+            this.txbTotalScore.Name = "txbTotalScore";
+            this.txbTotalScore.ReadOnly = true;
+            this.txbTotalScore.Size = new System.Drawing.Size(36, 20);
+            this.txbTotalScore.TabIndex = 5;
+            this.txbTotalScore.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 9;
+            // 
+            // lblRound
+            // 
+            this.lblRound.AutoSize = true;
+            this.lblRound.Location = new System.Drawing.Point(125, 16);
+            this.lblRound.Name = "lblRound";
+            this.lblRound.Size = new System.Drawing.Size(39, 13);
+            this.lblRound.TabIndex = 15;
+            this.lblRound.Text = "Round";
+            // 
+            // txbRound
+            // 
+            this.txbRound.Location = new System.Drawing.Point(170, 12);
+            this.txbRound.Name = "txbRound";
+            this.txbRound.ReadOnly = true;
+            this.txbRound.Size = new System.Drawing.Size(36, 20);
+            this.txbRound.TabIndex = 16;
+            this.txbRound.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Current Score";
+            // 
+            // txbCurrentScore
+            // 
+            this.txbCurrentScore.Location = new System.Drawing.Point(291, 13);
+            this.txbCurrentScore.Name = "txbCurrentScore";
+            this.txbCurrentScore.ReadOnly = true;
+            this.txbCurrentScore.Size = new System.Drawing.Size(36, 20);
+            this.txbCurrentScore.TabIndex = 18;
+            this.txbCurrentScore.Text = "0";
+            // 
+            // txbResult
+            // 
+            this.txbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbResult.Location = new System.Drawing.Point(133, 64);
+            this.txbResult.Name = "txbResult";
+            this.txbResult.ReadOnly = true;
+            this.txbResult.Size = new System.Drawing.Size(112, 26);
+            this.txbResult.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(133, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Answer";
+            // 
+            // btnFindResult
+            // 
+            this.btnFindResult.Location = new System.Drawing.Point(216, 95);
+            this.btnFindResult.Name = "btnFindResult";
+            this.btnFindResult.Size = new System.Drawing.Size(75, 23);
+            this.btnFindResult.TabIndex = 21;
+            this.btnFindResult.Text = "Find Result";
+            this.btnFindResult.UseVisualStyleBackColor = true;
+            this.btnFindResult.Click += new System.EventHandler(this.btnFindResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 141);
+            this.Controls.Add(this.btnFindResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txbResult);
+            this.Controls.Add(this.txbCurrentScore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbRound);
+            this.Controls.Add(this.lblRound);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbTotalScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAnagram);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVowel);
+            this.Controls.Add(this.btnConsonant);
+            this.Name = "Form1";
+            this.Text = "Countdown Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConsonant;
+        private System.Windows.Forms.Button btnVowel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAnagram;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbTotalScore;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblRound;
+        private System.Windows.Forms.TextBox txbRound;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbCurrentScore;
+        private System.Windows.Forms.TextBox txbResult;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnFindResult;
+    }
+}
+
---
+++ Countdown Game/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+
+namespace Countdown_Game
+{
+    public partial class Form1 : Form
+    {
+        List<string> vowels = new List<string>();
+        List<string> consonants = new List<string>();
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+
+        private int MAX_ROUND = 4;
+        private string vowelPatten = "[aeiou]";
+        private string dictionary = @"C:\Users\OEM\Desktop\C# Projects\Dictionary.txt";
+        private string anagramList = @"C:\Users\OEM\Desktop\C# Projects\anagram.txt";
+
+        private string playAnotherGame = "Would you like to play another game?";
+        private string pressOkOrCancel = "Press Ok to continue or Cancel to exit the program.";
+        private string pressOkToExit = "Press Ok to exit the game.";
+        private string play4GamesAlready = "OMG! You have played 4 games already!";
+
+        int currentRound = 0;
+        int totalScore = 0;
+
+        string anagram = "";
+       
+        Regex regex;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //get random anagram from the list anagram.txt
+            anagram = getRandomAnagram();
+
+            //create a vowel patten to identify vowels from anagram.
+            regex = new Regex(vowelPatten);
+
+            //split a word into two parts; vowels and consonants 
+            splitVowelsAndConsonant(anagram);
+
+            //on the form, just make it easy for the users to see the number of characters (vowels or consonants) left.
+            updateLeftCharacters();
+        }
+        //This method checks the provided anagram against words from the dictionary.txt
+        //In this little program, I made this method to pick the correct one unless user did not finish picking vowels or consonants.
+        //e.g we have an anagram of 6 characters but, we only pick 3 or 5 characters. In this case, the program will pick the most appropriate word.
+        private void findLongestWord()
+        {
+            String line;
+            StreamReader file = new StreamReader(dictionary);
+
+            //loop through the dictionary.txt file to find the most appropriate word.
+            while ((line = file.ReadLine()) != null)
+            {
+                string properWord = line;
+                string anagram = txtAnagram.Text;
+                //find out how difference between anagram and word from dictionary.
+                checkTheDifference(anagram, properWord);
+            }
+
+            //for every word in the dictionary, I added them to a hashmap with key is the 'proper word' and value is the 'number of difference character'
+            //e.g anagram = elhho and properWord = hello. So, the number of difference character between them is 5.
+            //Then I iterate through the hashmap and grap the highest value.
+            int tempLongestWord = dict.Values.Max();
+            foreach (KeyValuePair<string, int> entry in dict)
+            {
+                if (entry.Value == tempLongestWord)
+                {
+                    txbResult.Text = entry.Key;
+                    txbCurrentScore.Text = tempLongestWord.ToString();
+                    totalScore += tempLongestWord;
+                    dict.Clear();
+                    break;
+                }
+            }
+        }
+        //This method used to find the number(score) of difference between anagram and proper word.
+        private int checkTheDifference(string anagram, string properWord)
+        {
+            String tempProperWord = properWord;
+            int score = 0;
+            int numberOfDifference = 0;
+
+            for (int i = 0; i < anagram.Length; i++)
+            {
+                if (anagram.Length >= properWord.Length)
+                {
+                    for (int z = 0; z < tempProperWord.Length; z++)
+                    {
+                        if (anagram[i].Equals(tempProperWord[z]))
+                        {
+                            //to find the number of difference, for every removal successfully, I added 1 to numberOfDiffernce variable
+                            tempProperWord = tempProperWord.Remove(z, 1);
+                            numberOfDifference++;
+                            break;
+                        }
+                    }
+                }
+            }
+            //Avoid adding duplicated key
+            if (!dict.ContainsKey(properWord))
+                dict.Add(properWord, numberOfDifference);
+            return score;
+        }
+        
+        //get random anagram from the list.
+        private string getRandomAnagram()
+        {
+            string word;
+            string[] lines = File.ReadAllLines(anagramList);
+            Random random = new Random();
+            word = lines[random.Next(lines.Length)];
+            return word;
+        }
+
+        //start new game and reset some of the fields such as current score, but keep the total score.
+        //provide message box for user to make decision whether keep playing or exit the program.
+        private void startNewGame()
+        {
+            MessageBoxResult messageBox = MessageBox.Show(pressOkOrCancel, playAnotherGame, MessageBoxButton.OKCancel);
+            if (messageBox == MessageBoxResult.Cancel)
+                this.Close();
+            else if (messageBox == MessageBoxResult.OK)
+            {
+                //if reach 4 rounds then exit the program.
+                //keep playing game is not good for your health.
+                if (currentRound == MAX_ROUND)
+                {
+                    MessageBoxResult messageReached4Games = MessageBox.Show(pressOkToExit, play4GamesAlready, MessageBoxButton.OK);
+                    if (messageReached4Games == MessageBoxResult.OK)
+                        this.Close();
+                }
+                else
+                    resetGame();
+            }
+        }
+
+        //Reset some fields back to normal including get new anagram etc...
+        private void resetGame()
+        {
+            txbCurrentScore.Text = "0";
+            txtAnagram.Text = "";
+            txbResult.Text = "";
+            //because, I made the consonant and vowel buttons to disable stage when there is no character left.
+            //Thus, when a new game started, I switched these button back to normal stage.
+            btnConsonant.Enabled = true;
+            btnVowel.Enabled = true;
+            anagram = getRandomAnagram();
+            splitVowelsAndConsonant(anagram);
+            updateLeftCharacters();
+
+        }
+
+        //Allow user to play up to 4 rounds then exit the game.
+        private void newRound()
+        {
+            if (currentRound == MAX_ROUND)
+            {
+                MessageBoxResult m = MessageBox.Show(pressOkToExit, play4GamesAlready, MessageBoxButton.OK);
+                if (m == MessageBoxResult.OK)
+                    this.Close();
+            }
+            else
+                currentRound++;
+            txbRound.Text = currentRound.ToString();
+        }
+
+        //split an anagram into 2 parts; vowels and consonant for an ease when click the vowel or consonant button.
+        private void splitVowelsAndConsonant(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                String temp = word[i].ToString();
+                //created a regex to detect vowels.
+                if (regex.IsMatch(temp))
+                    vowels.Add(temp);
+                else
+                    consonants.Add(temp);
+            }
+        }
+
+        //show the number of characters (vowel and consonant) left for users.
+        private void updateLeftCharacters()
+        {
+            btnVowel.Text = "vowels (" + vowels.Count() + ")";
+            btnConsonant.Text = "consonants (" + consonants.Count() + ")";
+        }
+
+        //get a consonant character and when there is no character left, the program switches this button to disable stage.
+        private void btnConsonant_Click(object sender, EventArgs e)
+        {
+            if (consonants.Count > 0)
+            {
+                txtAnagram.Text += consonants.ElementAt(0);
+                consonants.RemoveAt(0);
+                if (consonants.Count == 0)
+                {
+                    btnConsonant.Enabled = false;
+                }
+            }
+            updateLeftCharacters();
+        }
+
+        //Same as consonant button.
+        private void btnVowel_Click(object sender, EventArgs e)
+        {
+            if (vowels.Count > 0)
+            {
+                txtAnagram.Text += vowels.ElementAt(0);
+                vowels.RemoveAt(0);
+                if (vowels.Count == 0)
+                {
+                    btnVowel.Enabled = false;
+                }
+            }
+            updateLeftCharacters();
+        }
+
+        //Find the proper word based on anagram.
+        //Start new game and also checking for 4 rounds.
+        private void btnFindResult_Click(object sender, EventArgs e)
+        {
+            findLongestWord();
+            txbTotalScore.Text = totalScore.ToString();
+            newRound();
+            startNewGame();
+        }
+    }
+}
